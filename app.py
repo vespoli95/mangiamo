0796d7f839045349acae2ae0b86c7b4ab9e80462
vespoli95 <antvespoli@gmail.com>
2019-05-15 16:25:47 -0400

    api call and card layout testing

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+import requests
+
+#api key = dd2f8137
+
 app = Flask(__name__)
+url = "https://api.edamam.com/search?q=chicken&app_id=dd2f8137&app_key=8b0da9158df77edc612feef9e21d8b7f&from=0&to=10"
+response = requests.get(url)
+data = response.json()['hits']
+for key in data:
+    print(key['recipe']['label'])
 
 #confin mySQL
 # app.config['MYSQL_HOST'] = 'localhost'
@@ -20,7 +28,7 @@ def hello():
 
     # cur.close()
 
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
