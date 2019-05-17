e880dfb5ffdae58b453c7f28075de675cec941ec
vespoli95 <antvespoli@gmail.com>
2019-05-16 21:12:27 -0400

    implemented dynamic api search

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
-#api key = dd2f8137
-
 app = Flask(__name__)
-url = "https://api.edamam.com/search?q=chicken&app_id=dd2f8137&app_key=8b0da9158df77edc612feef9e21d8b7f&from=0&to=10"
-response = requests.get(url)
-data = response.json()['hits']
-for key in data:
-    print(key['recipe']['label'])
+app_key = '8b0da9158df77edc612feef9e21d8b7f'
+app_id = 'dd2f8137'
+result_count = '20'
+
+
+
+
 
 #confin mySQL
 # app.config['MYSQL_HOST'] = 'localhost'
@@ -17,10 +17,7 @@ for key in data:
 # app.config['MYSQL_DB'] = 'myflaskapp'
 # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # mysql = MySQL(app)
-
-@app.route("/")
-def hello():
-    # cur = mysql.connection.cursor()
+  # cur = mysql.connection.cursor()
     # cur.execute("INSERT INTO users(username, password, email, date_joined, post_count) VALUES(" +
     # "antvespoli, sword099, antvespoli@gmail.com, 20/20/1999, 20")
 
@@ -28,7 +25,21 @@ def hello():
 
     # cur.close()
 
-    return render_template('index.html', data=data)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    
+    if request.method == 'POST':
+        q = request.form['searchBar']
+        url = "https://api.edamam.com/search?q=" + q + "&app_id=" + app_id + "&app_key=" + app_key + "&from=0&to=" + result_count
+        print(url)
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()['hits']
+            for key in data:
+                print(key['recipe']['label'])
+            return render_template('index.html', data=data)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
