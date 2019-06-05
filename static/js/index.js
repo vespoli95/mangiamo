c2d2fd32d885d5d909fe824a81cb11bf950f90e3
vespoli95 <antvespoli@gmail.com>
2019-06-04 21:38:41 -0400

    ES6 test

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,43 @@
-$(document).ready(function(){
     let app_key = '8b0da9158df77edc612feef9e21d8b7f';
     let app_id = 'dd2f8137';
     let from = 0;
     let to = 25;
+    let recipeSearch = document.getElementById('tbRecipeSearch');
+    let loader = document.getElementById('loader');
+
 
     function apiCall(appending){
 
-        if ($('#tbRecipeSearch').val().length > 1) {
+        if (recipeSearch.value.length > 1) {
     
-            let q = "https://api.edamam.com/search?q=" + $('#tbRecipeSearch').val();
+            let q = "https://api.edamam.com/search?q=" + recipeSearch.value;
             let num_results = "&from=" + from + "&to=" + to;
-            let num_ingredients = $('#tbNumIngredients').val() ? "&ingr=" + $('#tbNumIngredients').val() : "";
-    
+            let num_ingredients =  document.getElementById('tbNumIngredients') ? "&ingr=" 
+                +  document.getElementById('tbNumIngredients') : "";
             let url =  q + "&app_id=" + app_id + "&app_key=" + app_key + num_results + num_ingredients;
             
-            //$('#loader').empty().append("<img src='https://media3.giphy.com/media/sSgvbe1m3n93G/200w.webp?cid=790b76115ced8e3e567842422e986b52&rid=200w.webp' alt='loader'>")
+            loader.innerHTML = "<img src='https://media3.giphy.com/media/sSgvbe1m3n93G/200w.webp?cid=790b76115ced8e3e567842422e986b52&rid=200w.webp' alt='loader'>";
+            
             fetch(url)
             .then((res) => res.json())
             .then((data) => {
                 hits = data['hits'];
-                console.log(hits);
-                recipes = document.querySelector('#recipes');
+                recipes = document.getElementById('recipes');
                 if (!appending)
-                    recipes.removeChild();
-                //console.log(recipes);
+                    recipes.removeChild(recipes.firstChild);
+                    for (let i = 0; i < hits.length; i++){
+                        recipes.innerHTML += 
+                        `<div class='card-body' onclick='window.location = "${hits[i]['recipe']['url']}"'>
+                            <img src='${hits[i]['recipe']['image']}' class='card-img-top' alt=''>
+                            <h5 class='card-title'>  ${hits[i]['recipe']['label']} </h5>
+                            <p>Ingredients:   ${hits[i]['recipe']['ingredients'].length}</p>
+                        </div>`
+                    }
+                    from += 25;
+                    to += 25;
             });
+            
+            loader.innerHTML = "";
 
             // $.ajax({
             //     type: 'GET',
@@ -54,25 +67,24 @@ $(document).ready(function(){
         }
     }
     //on change
-    $('#tbRecipeSearch').on('change', function(){
+     recipeSearch.addEventListener('change', e => {
         apiCall(false);
     });
     //on click
-    $('#btnRecipeSearch').on('click', function(){
+    document.getElementById('btnRecipeSearch').addEventListener('click', e => {
         apiCall(false);
     });
     //on enter
-    $('#tbRecipeSearch').on('keyup', function(e){
+     recipeSearch.addEventListener('keyup', e => {
         if (e.keyCode === 13){
             e.preventDefault();
             apiCall(false);
         }
     })
-    //on scroll to bottom
-    $(window).scroll(function() {
-        if($(window).scrollTop() == $(document).height() - $(window).height()) {
-            apiCall(true);
-        }
+    ////on scroll to bottom
+    // $(window).scroll(function() {
+    //     if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    //         apiCall(true);
+    //     }
         
-    });
-});
+    // });
