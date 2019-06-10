1bf51ac713d6f06591b6e0de7f63fb9aac317335
vespoli95 <antvespoli@gmail.com>
2019-06-10 11:16:04 -0400

    user class

---
+++ user.py
@@ -0,0 +1,9 @@
+class user():
+    def __init__(self, email, password, date_joined, first_name, last_name, age, country):
+        self.email = email
+        self.password = password
+        self.date_joined = date_joined
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.country = country
